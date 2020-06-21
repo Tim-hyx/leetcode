28098ac08ea8fea0131c2031dce69c47316b66b2
Yuxuan Huang <54028895+huangyuxuan1997@users.noreply.github.com>
2020-06-21 14:37:35 +1000

    Create #21.py

---
+++ #21.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 is None and l2:
+            return l2
+        if l2 is None and l1:
+            return l1
+        if l1 and l2:
+            if l1.val < l2.val:
+                l1.next = self.mergeTwoLists(l1.next, l2)
+                return l1
+            else:
+                l2.next = self.mergeTwoLists(l1, l2.next)
+                return l2
