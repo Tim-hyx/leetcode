bbab116a5f06a58db56d868dd6a3376c298ef4d1
Yuxuan Huang <54028895+huangyuxuan1997@users.noreply.github.com>
2020-06-21 15:39:53 +1000

    Update #242.py

--- #242.py
+++ #242.py
@@ -1,10 +1,10 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        new_list1=[i for i in s]
+        new_list1 = [i for i in s]
         new_list2 = [j for j in t]
         new_list1.sort()
         new_list2.sort()
-        if new_list1==new_list2:
+        if new_list1 == new_list2:
             return True
         else:
             return False
