4b497541df1cff931c5321955848a64f9c02fce0
Tim-hyx <54028895+Tim-hyx@users.noreply.github.com>
2020-08-11 20:58:19 +1000

    Update #179.py

--- #179.py
+++ #179.py
@@ -1,3 +1,4 @@
+# first method
 class LargerNumKey(str):
     def __lt__(x, y):
         return x + y > y + x
@@ -7,3 +8,18 @@ class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         largest_num = ''.join(sorted(map(str, nums), key=LargerNumKey))
         return '0' if largest_num[0] == '0' else largest_num
+
+
+# second method
+from functools import cmp_to_key
+
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = list(map(str, nums))
+        cmp = cmp_to_key(lambda x, y: int(y + x) - int(x + y))
+        largestNumber = ''.join(sorted(nums, key=cmp))
+        if largestNumber[0] == '0':
+            return '0'
+        else:
+            return largestNumber
