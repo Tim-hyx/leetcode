0d3bed0538a6708aa8d0f3b46397c9c159f2624f
Tim-hyx <54028895+Tim-hyx@users.noreply.github.com>
2020-08-15 17:38:20 +1000

    Create #1502.py

---
+++ #1502.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr.sort()
+        difference = arr[0] - arr[1]
+        for i in range(1, len(arr) - 1):
+            if arr[i] - arr[i + 1] != difference:
+                return False
+        return True
