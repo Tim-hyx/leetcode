e78d52d722756be896c68d40ad02e5255c5ac988
Tim-hyx <54028895+Tim-hyx@users.noreply.github.com>
2021-03-04 21:30:38 +0800

    Create #354.py

---
+++ #354.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        if not envelopes:
+            return 0
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        dp = [1] * len(envelopes)
+        for i in range(len(envelopes)):
+            for j in range(i):
+                if envelopes[j][1] < envelopes[i][1]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
